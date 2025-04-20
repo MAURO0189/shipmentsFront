dfb49f1094c291b5ae1a6fa87023775d25bcc95d
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2025-04-20 16:25:19 -0500

    add new routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,10 +4,10 @@ import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import Register from "../features/auth/pages/Register";
 import Login from "../features/auth/pages/Login";
-import HomeUser from "../pages/HomeUser";
 import Loading from "../components/Loading";
 import AdminLogin from "../admin/pages/AdminLogin";
 import AdminDashboard from "../admin/components/AdminDashboard";
+import UserLayout from "../pages/UserLayout";
 
 const AppRouter = () => {
   return (
@@ -19,7 +19,7 @@ const AppRouter = () => {
       <Route path="/loading" element={<Loading />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/homeUser" element={<HomeUser />} />
+      <Route path="/userLayout/*" element={<UserLayout />} />
     </Routes>
   );
 };
