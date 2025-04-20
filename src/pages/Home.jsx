0ef5271af334e1e746d01e1b4a103999ac81c28b
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2025-04-20 13:37:02 -0500

    deleted links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
-import TrackingSection from "../components/TrackingSection";
 import ServicesSection from "../components/ServicesSection";
 import StatsSection from "../components/StatsSection";
 import TestimonialsSection from "../components/TestimonialsSection";
@@ -13,7 +12,6 @@ const Home = () => {
       <Header />
       <main className="flex-grow">
         <HeroSection />
-        <TrackingSection />
         <ServicesSection />
         <StatsSection />
         <TestimonialsSection />
